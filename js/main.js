257b651ddb5b1778ed06d4f927aed25fd78cccc5
Mahdy Khairudin <mr.mahdy5@gmail.com>
2022-07-13 15:13:09 +0700

    perbaikan

--- js/main.js
+++ js/main.js
@@ -235,27 +235,32 @@
 		$('#buka-undangan').click(function () {
 			$("#kartu").fadeOut("slow");
 			$("body").css('overflow', 'visible');
+			const song1 = document.getElementById("ibu");
+			togglePlay.src = "audio/ibu.mp3"
+			togglePlay.alt = "play"
+			document.getElementById("playbtn").src = "images/pause.png";
+			song1.play();
 		})
 	});
 
 
 
 	// disable klik kanan 
-	document.addEventListener('contextmenu', (e) => e.preventDefault());
+	// document.addEventListener('contextmenu', (e) => e.preventDefault());
 
-	function ctrlShiftKey(e, keyCode) {
-		return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
-	}
+	// function ctrlShiftKey(e, keyCode) {
+	// 	return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+	// }
 
-	document.onkeydown = (e) => {
-		// Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
-		if (
-			event.keyCode === 123 ||
-			ctrlShiftKey(e, 'I') ||
-			ctrlShiftKey(e, 'J') ||
-			ctrlShiftKey(e, 'C') ||
-			(e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
-		)
-			return false;
-	};
+	// document.onkeydown = (e) => {
+	// 	// Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+	// 	if (
+	// 		event.keyCode === 123 ||
+	// 		ctrlShiftKey(e, 'I') ||
+	// 		ctrlShiftKey(e, 'J') ||
+	// 		ctrlShiftKey(e, 'C') ||
+	// 		(e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+	// 	)
+	// 		return false;
+	// };
 }());
